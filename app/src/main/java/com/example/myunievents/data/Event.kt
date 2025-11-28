d2eab290ef465d213dfd9dad91fb0a611cf8994c
Dauud10 <k00263519@student.tus.ie>
2025-11-28 01:55:26 +0000

    Added Event data model for Room database

---
+++ app/src/main/java/com/example/myunievents/data/Event.kt
@@ -0,0 +1,14 @@
+package com.example.myunievents.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "events")
+data class Event(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val eventName: String,
+    val eventDate: String,
+    val eventTime: String,
+    val location: String
+)
