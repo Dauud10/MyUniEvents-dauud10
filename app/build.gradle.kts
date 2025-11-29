9708381a2812111f386eb4eb1f5bcd26880eee1d
Dauud10 <k00263519@student.tus.ie>
2025-11-29 20:00:16 +0000

    Added Navigation Compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
+
+    // Navigation for Jetpack Compose
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
 }
